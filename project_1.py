bf46f3290dd04d323d1140bd7d1303721852e3dc
claireaf-crypto <claireaf@umich.edu>
2025-10-10 17:07:16 -0500

    added species to group_island_by_sex and calculated percentage of gender and species

--- project_1.py
+++ project_1.py
@@ -13,25 +13,66 @@ def load_penguins(filename):
     #print (data)
     return data
 
-#Anna Kerhouslas calculation 1
+#Anna Kerhouslas calculations:
 
+#Abbey Halabis Calculations:
+
+
+#Claire Fuller Calculations:
 #--------Helper Functions for calculate gender distrubution----------Claire Fuller
-
-def group_island_by_sex(penguins): # group penguin by island and gender occurance
-    count = {}
+#gender occurance grouping functions 
+def group_island_by_sex(penguins): # group penguin by island and gender and species occurance
+    counts = {}
     for p in penguins:
+        species = p.get("species", "Unknown")
         island = p.get("island", 'Unknown')
         sex = (p.get("sex") or "Unknown").capitalize() 
-        if island not in count:
-            count[island] = {"Male": 0, "Female": 0, "Unknown": 0}
 
-        if sex not in count[island]:
-            count[island][sex] = 0 #mkaing sure the key exsists for na vlaues am i supposed t put this as a test case???
-            count[island][sex] += 1
+        if species not in counts:
+            counts[species] = {}
+        
+        if island not in counts:
+            counts[island] = {"Male": 0, "Female": 0, "Unknown": 0}
 
-    return count
+        if sex not in counts[island]:
+            counts[island][sex] = 0 #mkaing sure the key exsists for na vlaues am i supposed t put this as a test case???
+        counts[species][island][sex] += 1
 
- def ca                            
+    return counts
 
-#hello
+#convert the genders into percentages
+def calculate_percentage(counts_dict):
+    percentages = {}
+
+    for species, island_data in counts_dict.items():
+        percentages[species] = {}
+
+        for island, counts in counts_dict.items():
+            total = sum(counts.values())
+
+            if total == 0:
+                island_percentages = {}
+                for gender in counts:
+                    island_percentages[gender] = 0
+                percentages[island] = island_percentages
+                continue
+    
+        island_percentages = {}
+        for gender, count in counts.items():
+            percent = (count / total ) * 100
+            island_percentages[gender] = percent
+
+        percentages[species][island] = island_percentages
+    
+
+    return percentages
+
+def 
+
+
+
+
 #split up the functions that me and my group are doing
+
+#call functions
+#def main():
