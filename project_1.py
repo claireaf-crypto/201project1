450f28c2f7decf2405bac3c99e2259ece451272d
claireaf-crypto <claireaf@umich.edu>
2025-10-10 16:42:46 -0500

    first helper function group_island_by_sex

--- project_1.py
+++ project_1.py
@@ -15,13 +15,23 @@ def load_penguins(filename):
 
 #Anna Kerhouslas calculation 1
 
+#--------Helper Functions for calculate gender distrubution----------Claire Fuller
 
-#--------Helper Functions for calculate gender distrubution----------
-
-def group_island_by_sex(penguins):
+def group_island_by_sex(penguins): # group penguin by island and gender occurance
     count = {}
     for p in penguins:
         island = p.get("island", 'Unknown')
-        sex = p.get(sex", 
+        sex = (p.get("sex") or "Unknown").capitalize() 
+        if island not in count:
+            count[island] = {"Male": 0, "Female": 0, "Unknown": 0}
+
+        if sex not in count[island]:
+            count[island][sex] = 0 #mkaing sure the key exsists for na vlaues am i supposed t put this as a test case???
+            count[island][sex] += 1
+
+    return count
+
+ def ca                            
+
 #hello
 #split up the functions that me and my group are doing
