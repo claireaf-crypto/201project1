1f54eba2658383b12f64406c05cb87add3f2a71f
claireaf-crypto <claireaf@umich.edu>
2025-10-10 16:38:44 -0400

    Add files via upload

---
+++ project_1.py
@@ -0,0 +1,21 @@
+import csv
+import os
+
+#Reading CSV into a list of dicts
+def load_penguins(filename):
+    data = []
+    dir_path = os.path.dirname(__file__)
+    file_path = os.path.join(dir_path,filename)
+    with open(file_path, mode = 'r', newline='') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        for row in csv_reader:
+            data.append(row)
+    #print (data)
+    return data
+
+#Anna Kerhouslas calculation 1
+
+
+#calculate gender distributuion
+#size vs island 
+#split up the functions that me and my group are doing
